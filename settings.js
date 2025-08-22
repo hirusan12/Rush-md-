8398b25b572e0def1c01e1584832208a515865dc
hirusan12 <aakifzain111@gmail.com>
2025-08-22 14:58:06 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "VISPER-MD&K4ZEACgD#E_HsxJivTSmONfngwCanrgb7mpUChHkvmZ-22R1nO0Y",
+    SESSION_ID: process.env.SESSION_ID || "VISPER-MD&Og5ngTwS#rvImL0DQeQy2c8QtsLLuIYUWKtY2idXf46UmkjijpmQ",
     // add your Session Id make sure it starts with lite~
 
     PREFIX: process.env.PREFIX || ".",
